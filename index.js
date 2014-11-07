582d4a3267458004b3deb057f35c729ef7837244
Felix Zapata <felix.zapata@adesis.com>
2014-11-07 14:00:00 +0100

    fix(data): create the data with requests and responses for stubby

--- index.js
+++ index.js
@@ -14,8 +14,86 @@ var _ = require('lodash'),
     gutil = require('gulp-util'),
     Stubby = require('stubby').Stubby,
     path = require('path'),
+    YAML = require('js-yaml'),
     PLUGIN_NAME = 'gulp-stubby';
 
+
+// defines the absolute path for external static request/response
+// files that will be processed internally by Stubby
+
+function setPathStaticFiles(array, filepath) {
+    filepath = path.dirname(filepath);
+
+
+
+    function setAbsoluteFilePath(file) {
+        return filepath + '/' + file;
+    }
+
+    array = array.map(function (object) {
+        if (_.isObject(object.request) && object.request.file) {
+            if (!isPathAbsolute(object.request.file)) {
+                object.request.file = setAbsoluteFilePath(object.request.file);
+            }
+        }
+        if (_.isObject(object.response)) {
+            // support collections for responses
+            if (_.isArray(object.response)) {
+                object.response = object.response.map(function (response) {
+                    if (response.file && !isPathAbsolute(response.file)) {
+                        response.file = setAbsoluteFilePath(response.file);
+                    }
+                    return response;
+                });
+            } else {
+                if (object.response.file && !isPathAbsolute(object.response.file)) {
+                    object.response.file = setAbsoluteFilePath(object.response.file);
+                }
+            }
+        }
+
+        return object;
+    });
+
+    return array;
+}
+
+function isPathAbsolute() {
+    var filepath = path.join.apply(path, arguments);
+    return path.resolve(filepath) === filepath.replace(/[\/\\]+$/, '');
+}
+
+function getAbsolutePath(filepath) {
+    if (isPathAbsolute(filepath)) {
+        filepath = process.cwd() + '/' + filepath;
+    }
+    return filepath;
+}
+
+
+function readYAML(filepath, options) {
+    var src = fs.readFileSync(filepath, options);
+    var result;
+    gutil.log('Parsing ' + filepath + '...');
+    try {
+        result = YAML.load(src);
+        return result;
+    } catch (e) {
+        throw new gutil.PluginError(PLUGIN_NAME, 'Unable to parse "' + filepath + '" file (' + e.problem + ').', e);
+    }
+}
+
+function readJSON(filepath, options) {
+    var src = fs.readFileSync(filepath, options);
+    var result;
+    gutil.log('Parsing ' + filepath + '...');
+    try {
+        result = JSON.parse(src);
+        return result;
+    } catch (e) {
+        throw new gutil.PluginError(PLUGIN_NAME, 'Unable to parse "' + filepath + '" file (' + e.message + ').', e);
+    }
+}
 
 function stubbyPlugin(customOptions) {
 
@@ -41,6 +119,45 @@ function stubbyPlugin(customOptions) {
         files = [];
 
 
+    function processDataForStubby() {
+        // Iterate over all specified file groups.
+        var data = _.union.apply(_, options.files.map(function (filepath) {
+            // Concat specified files.
+            var data;
+            // Read file source.
+            if (/.yaml$/g.test(filepath)) {
+                data = readYAML(filepath);
+            } else if (/.js$/g.test(filepath)) {
+                try {
+                    data = require(filepath);
+                } catch (e) {
+                    throw new gutil.PluginError(PLUGIN_NAME, 'Error while parsing JS file "' + filepath + '"', e);
+                }
+            } else {
+                data = readJSON(filepath);
+            }
+
+            if (!_.isArray(data)) {
+                data = [data];
+            }
+
+            return options.relativeFilesPath ? setPathStaticFiles(data, filepath) : data;
+
+        }));
+
+
+
+        if (_.isObject(options.data)) {
+            if (_.isArray(options.data)) {
+                options.data = _.union(options.data, data);
+            } else {
+                options.data = data.push(options.data);
+            }
+        } else {
+            options.data = data;
+        }
+    }
+
     function startStubby() {
 
         var stubbyServer = new Stubby();
@@ -61,7 +178,7 @@ function stubbyPlugin(customOptions) {
             gutil.log('Admin server listening on port ' + options.admin);
 
             if (!options.persistent) {
-                 done();
+                done();
             }
 
         });
@@ -96,7 +213,7 @@ function stubbyPlugin(customOptions) {
         if (files.length) {
             options.files = files;
         }
-
+        processDataForStubby();
         startStubby();
     }
 
